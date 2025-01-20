8f0662b06c8a1b264f5ae371b077888e8dbae5c8
RD3V <47334312+sirdx@users.noreply.github.com>
2025-01-20 20:42:03 +0100

    test: add some basic client tests

--- Pegasustan.Tests/PegasusClientTest.cs
+++ Pegasustan.Tests/PegasusClientTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Pegasustan.Tests;
+﻿using Pegasustan.Domain;
+
+namespace Pegasustan.Tests;
 
 [TestFixture]
 public sealed class PegasusClientTest
@@ -10,11 +12,77 @@ public sealed class PegasusClientTest
     {
         _pegasusClient = await PegasusClient.CreateAsync();
     }
+    
+    [Test]
+    public void GetLanguagesAsync_WhenCalled_ReturnsNonEmptyArray()
+    {
+        var languages = Array.Empty<Language>();
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            languages = await _pegasusClient.GetLanguagesAsync();
+        });
+        Assert.That(languages, Is.Not.Empty);
+    }
 
     [Test]
-    public void MakeSureDefaultLanguageIsEnglish()
+    public void DefaultLanguage_WhenUnchanged_IsEnglish()
     {
         Assert.That(_pegasusClient.DefaultLanguage.Code.ToLower(), Is.EqualTo("en"));
+    }
+    
+    [TestCase("tr")]
+    [TestCase("en")]
+    [TestCase("de")]
+    [TestCase("fr")]
+    [TestCase("it")]
+    [TestCase("es")]
+    [TestCase("ru")]
+    [TestCase("ar")]
+    public void ChangeLanguage_WhenCodePassed_SetsDefaultLanguageToCode(string code)
+    {
+        _pegasusClient.ChangeLanguage(code);
+        Assert.That(_pegasusClient.DefaultLanguage.Code.ToLower(), Is.EqualTo(code));
+    }
+
+    [Test]
+    public void GetDepartureCountriesAsync_WhenCalled_ReturnsNonEmptyArray()
+    {
+        var countries = Array.Empty<Country>();
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            countries = await _pegasusClient.GetDepartureCountriesAsync();
+        });
+        Assert.That(countries, Is.Not.Empty);
+    }    
+
+    [Test]
+    public async Task GetArrivalCountriesAsync_WhenCalled_DoesNotThrow()
+    { 
+        var countries = await _pegasusClient.GetDepartureCountriesAsync();
+        Assert.DoesNotThrowAsync(async () => await _pegasusClient.GetArrivalCountriesAsync(countries.First().Ports.First()));
+    }
+        
+    [Test]
+    public void GetCitiesForBestDealsAsync_WhenCalled_ReturnsNonEmptyArray()
+    {
+        var cities = Array.Empty<BestDealsCity>();
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            cities = await _pegasusClient.GetCitiesForBestDealsAsync();
+        });
+        Assert.That(cities, Is.Not.Empty);
+    }
+    
+    [Test]
+    public async Task GetBestDealsAsync_WhenCalled_ReturnsNonEmptyArray()
+    {
+        var cities = await _pegasusClient.GetCitiesForBestDealsAsync();
+        var deals = Array.Empty<BestDeal>();
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            deals = await _pegasusClient.GetBestDealsAsync(cities.First(), Currency.Lira);
+        });
+        Assert.That(deals, Is.Not.Empty);
     }
     
     // TODO: More tests
